bd74c041747f8d5adf5f03a4a59494b20d6b037c
chenziwen <401030526@qq.com>
2020-09-21 09:56:03 +0800

    1.编写了前端注册窗口，在注册窗口测试了登陆功能，返回结果有问题，不能达到目标效果

--- loginpage/src/components/RegisterModel.vue
+++ loginpage/src/components/RegisterModel.vue
@@ -2,7 +2,7 @@
     <div>
         <!-- Button trigger modal -->
         <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-            Launch demo modal
+            注册账号
         </button>
 
         <!-- Modal -->
@@ -10,20 +10,55 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
                         <h4 class="modal-title" id="myModalLabel">用户注册</h4>
                     </div>
                     <div class="modal-body">
-                        <div>用户名</div>
-                        <div>密码</div>
-                        <div>密码确认</div>
+                        <div>用户名<label>
+                            <input type="text" v-model="userName">
+                        </label></div>
+                        <div>密码<label>
+                            <input type="password" v-model="password">
+                        </label></div>
+                        <div>密码确认<label>
+                            <input type="password" v-model="passwordconfirm">
+                        </label></div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">不注册了！关闭</button>
-                        <button type="button" class="btn btn-primary">提交注册</button>
+                        <button type="button" class="btn btn-primary" @click="submitInfo">提交注册</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+    export default {
+        name:'RegisterModel',
+        data(){
+            return{
+                userName:'',
+                password:'',
+                passwordconfirm:'',
+            }
+        },
+        methods:{
+           submitInfo() {
+               let _this=this;
+               // alert("用户名:"+_this.userName+"密码:"+_this.password+"密码确认:"+_this.passwordconfirm);
+               this.$http.post("http://localhost:9005/user/login",{userName:_this.userName,password:_this.password})
+                   .then(function (response){
+                       alert("返回结果："+response.toString());
+                       if (response==='1'){
+                           alert('登陆成功');
+                       }else {
+                           alert('登陆失败');
+                       }
+                   })
+               }
+
+           }
+    }
+</script>
