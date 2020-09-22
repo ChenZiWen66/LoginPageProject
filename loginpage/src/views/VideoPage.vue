ce503a6a6a54e7f342d496c4dd0e1b30dba4e15d
chenziwen <401030526@qq.com>
2020-09-22 12:51:15 +0800

    1.编写了视频界面的基本模型

--- loginpage/src/views/VideoPage.vue
+++ loginpage/src/views/VideoPage.vue
@@ -1,6 +1,98 @@
 <template>
     <div>
-        视频界面
+        <div class="row">
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <img src="../../public/twly.jpg" alt="../../public/xiaolan.jpg">
+                    <div class="caption">
+                        <h3>贪玩蓝月</h3>
+                        <p>是兄弟就来砍我！！是兄弟就来砍我！</p>
+                        <p>
+                            <button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <img src="../../public/twly.jpg" alt="../../public/xiaolan.jpg">
+                    <div class="caption">
+                        <h3>贪玩蓝月</h3>
+                        <p>是兄弟就来砍我！！是兄弟就来砍我！</p>
+                        <p>
+                            <button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <img src="../../public/twly.jpg" alt="../../public/xiaolan.jpg">
+                    <div class="caption">
+                        <h3>贪玩蓝月</h3>
+                        <p>是兄弟就来砍我！！是兄弟就来砍我！</p>
+                        <p>
+                            <button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <img src="../../public/twly.jpg" alt="../../public/xiaolan.jpg">
+                    <div class="caption">
+                        <h3>贪玩蓝月</h3>
+                        <p>是兄弟就来砍我！！是兄弟就来砍我！</p>
+                        <p>
+                            <button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <img src="../../public/twly.jpg" alt="../../public/xiaolan.jpg">
+                    <div class="caption">
+                        <h3>贪玩蓝月</h3>
+                        <p>是兄弟就来砍我！！是兄弟就来砍我！</p>
+                        <p>
+                            <button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="thumbnail">
+                    <img src="../../public/twly.jpg" alt="../../public/xiaolan.jpg">
+                    <div class="caption">
+                        <h3>贪玩蓝月</h3>
+                        <p>是兄弟就来砍我！！是兄弟就来砍我！</p>
+                        <p>
+                            <button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,3 +101,6 @@
         name: "VideoPage"
     }
 </script>
+<style>
+
+</style>
